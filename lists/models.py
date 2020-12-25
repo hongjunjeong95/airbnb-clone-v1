959ccecbc61aa2dfdbc80dad472e1b5c0ac6c97f
zpskek <wjdghdwns0@gmail.com>
2020-12-25 15:36:33 +0900

    Conversations, Lists, Reservations Admin

--- lists/models.py
+++ lists/models.py
@@ -14,3 +14,9 @@ class List(core_models.TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+    def count_rooms(self):
+        print(self.rooms)
+        return self.rooms.count()
+
+    count_rooms.short_description = "Number of Rooms"
