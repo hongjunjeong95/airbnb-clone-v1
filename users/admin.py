d41bc5db2933540403b16ceed6e87ef2678ab4e1
zpskek <wjdghdwns0@gmail.com>
2020-12-25 16:28:00 +0900

    Configuring User Uploads

--- users/admin.py
+++ users/admin.py
@@ -10,7 +10,7 @@ class CustomUserAdmin(UserAdmin):
 
     fieldsets = UserAdmin.fieldsets + (
         (
-            "Banana",
+            "Custom Profile",
             {
                 "fields": (
                     "avatar",
