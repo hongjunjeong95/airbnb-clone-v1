c1c2b45f615748225373dd6ee8d79dc1acd26b00
zpskek <wjdghdwns0@gmail.com>
2020-12-27 17:43:30 +0900

    seed_lists

--- reviews/management/commands/seed_reviews.py
+++ reviews/management/commands/seed_reviews.py
@@ -1,7 +1,7 @@
 import random
 from django.core.management.base import BaseCommand
 from django_seed import Seed
-from reviews import models as review_model
+from reviews import models as review_models
 from users import models as user_models
 from rooms import models as room_models
 
@@ -24,7 +24,7 @@ class Command(BaseCommand):
         users = user_models.User.objects.all()
         rooms = room_models.Room.objects.all()
         seeder.add_entity(
-            review_model.Review,
+            review_models.Review,
             number,
             {
                 "accuracy": lambda x: random.randint(0, 6),
