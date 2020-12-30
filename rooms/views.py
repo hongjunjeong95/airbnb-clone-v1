9cf1eee160387680d9e4536302d529d829227b26
zpskek <wjdghdwns0@gmail.com>
2020-12-30 09:31:07 +0900

    Template, Form, Url, Setup

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 
@@ -42,3 +43,9 @@ class RoomDetail(DetailView):
 
     model = models.Room
     # pk_url_kwarg = "pk"
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
