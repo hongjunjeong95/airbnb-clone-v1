b666fd119b78ea474ccf9fe44b942f3db408b958
zpskek <wjdghdwns0@gmail.com>
2021-01-02 08:12:38 +0900

    Signup Form

--- users/views.py
+++ users/views.py
@@ -11,7 +11,6 @@ class LoginView(FormView):
     template_name = "users/login.html"
     form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")
-    initial = {"email": "wjdghdwns0@gmail.com"}
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -40,3 +39,23 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "Hong Jun",
+        "last_name": "Jeong",
+        "email": "wjdghdwns0@gmail.com",
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
