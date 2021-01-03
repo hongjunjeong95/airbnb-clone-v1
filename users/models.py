80f76ba185b3a327f54ca2e1910ced0bec66c86b
zpskek <wjdghdwns0@gmail.com>
2021-01-03 13:23:56 +0900

    Github Login

--- users/models.py
+++ users/models.py
@@ -34,6 +34,16 @@ class User(AbstractUser):
 
     CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGIN_KAKAO = "kakao"
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "Email"),
+        (LOGIN_GITHUB, "Github"),
+        (LOGIN_KAKAO, "Kakao"),
+    )
+
     avatar = models.ImageField(upload_to="avatar", blank=True)
     gender = models.CharField(choices=GENDER_CHOISES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
@@ -47,9 +57,13 @@ class User(AbstractUser):
     superhost = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
     email_secret = models.CharField(max_length=20, default="", blank=True)
+    login_method = models.CharField(
+        max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
+    )
 
     def verify_email(self):
-        print(self)
+        print(dir(self))
+        print(f"self.email : {self.email}")
         if self.email_verified is False:
             secret = uuid.uuid4().hex[:20]
             self.email_secret = secret
@@ -64,4 +78,5 @@ class User(AbstractUser):
                 fail_silently=False,
                 html_message=html_message,
             )
+            self.save()
         return
